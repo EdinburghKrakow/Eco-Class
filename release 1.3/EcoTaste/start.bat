7fe33e7d2f28880db223bd208a724302586afefb
chsuhw <166412354+chsuhw@users.noreply.github.com>
2026-05-28 21:35:33 +0700

    Delete release 1.3/EcoTaste directory

--- release 1.3/EcoTaste/start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Запуск сервера в новом окне
-start cmd /k "cd /d %USERPROFILE%\Desktop\EcoTaste\server && node server.js"
-
-:: Запуск фронтенда в новом окне
-start cmd /k "cd /d %USERPROFILE%\Desktop\EcoTaste && npm run dev"
-
-start http://localhost:5173
